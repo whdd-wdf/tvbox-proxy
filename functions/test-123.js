ecf666233671b179aafa19267512722201e8c410
TVBox Proxy <tvbox@local>
2026-05-01 11:56:34 +0800

    fix: 修复文件名转义问题，使用正确的 [[path]].js

--- functions/test-123.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response('<h1 style="color:red; font-size:30px;">🎉 SUCCESS! Functions are ALIVE!</h1><p>Time: ' + new Date().toISOString() + '</p>', {
-    headers: { 'Content-Type': 'text/html' }
-  });
-}
